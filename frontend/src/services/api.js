b55b312638d0d445f4e68b9de4dab197d9b6cd09
gauravkondhalkarrkinfynova-netizen <gauravkondhalkar.rkinfynova@gmail.com>
2026-01-20 15:44:49 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { refreshToken } from "./authService";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "",
   headers: {
     "Content-Type": "application/json",
   },
