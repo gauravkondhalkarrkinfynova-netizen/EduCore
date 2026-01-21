955a7839df07eae811e030030e76947b34ba1c0d
gauravkondhalkarrkinfynova-netizen <gauravkondhalkar.rkinfynova@gmail.com>
2026-01-22 01:11:46 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -18,7 +18,7 @@ const processQueue = (error, token = null) => {
 
 const API = axios.create({
   // baseURL: "/api",
-  baseURL: "http://3.7.212.22:3000"
+  baseURL: "https://3.7.212.22:3000"
 ,
   headers: {
     "Content-Type": "application/json",
